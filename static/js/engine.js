7c074bfe566916513da0e89b124066297ac12ac7
Erik Nygren <erik@sleepwalker.(none)>
2012-11-13 19:05:42 -0500

    Added region selector logic

--- static/js/engine.js
+++ static/js/engine.js
@@ -68,6 +68,72 @@ var $win = $(window),
         $from.change(update_payoff); 
         $to.change(update_payoff); 
     }, 
+    make_dest_selector = function($container) { 
+        var li_template = kendo.template('<li data-value=#value#><span>#value</span></li>'),
+            $region_ul = $('<ul></ul>'),
+            $city_div = $('<div></div>'),
+            i, il, odd_even, $odd_even_ul, $dice_ul, key, region_city, 
+            set_region, set_city; 
+
+        parse_roll = function($panel, dice_val_fn) { 
+            var is_odd = $panel.find("ul.odd-even > li:selected"), 
+                dice_val = $panel.find("ul.dice > li:selected"), 
+            if (!is_odd || !dice_val) { return; }
+            is_odd = (is_odd.val() === 'Odd'); 
+            dice_val = dice_val.val(); 
+            dice_val_fn(dice_val, is_odd); 
+        }; 
+
+        set_region = function() { 
+
+            parse_roll($('#regions'), function(dice_val, is_odd) { 
+                var region_val = dice_to_region(dice_val, is_odd); 
+
+                $region_ul.find('li').each(function() { 
+                    var $elem = $(this); 
+                    if ($elem.val() === region_val) { 
+                        $elem.attr("selected", "selected"); 
+                    } else { 
+                        $elem.removeAttr("selected"); 
+                    }
+                }); 
+            );
+        }; 
+        set_city = function() { 
+            parse_roll($('#city_panel'), function(dice_val, is_odd) { 
+                var city_val = dice_to_city(dice_val, is_odd); 
+                $('#city_greeting').text("You're going to "); 
+                $('#city').text(city_val); 
+            }); 
+        };
+
+        $city_div.append($("<span id='city-greeting'></span><span id='city'></span>"));
+        for (region_city = 'Region', region_city === 'Region'; region_city = 'City') { 
+            $odd_even_ul = $('<ul></ul>');
+            $dice_ul = $('<ul></ul>'); 
+            for (odd_even = 'Odd'; odd_even === 'Odd'; odd_even = 'Even') { 
+                $odd_even_ul.append($(kendo.render(li_template, {"value" : odd_even}))); 
+                for (i = 2; i < 13; i++) { 
+                    $dice_ul.append($(kendo.render(li_template, {'value' : i}))); 
+                }
+            }
+            $container.append($('h2').text(region_city));
+            $container.append($odd_even_ul); 
+            $container.append($dice_ul); 
+
+            if (region_city === 'Region') { 
+                for (key in regions) { 
+                    if (own_prop(regions, key) {    
+                        $region_ul.append(kendo.render(li_template, {"value" : key})); 
+                    }
+                }
+            } else {
+                $container.append($city_div);
+            }
+       }
+        $container.append($odd_even_ul); 
+        $container.append($dice_ul); 
+    },
     key_handlers = {},
     register_key = function(character, handler_fn) { 
         key_handlers[character[0].toUpperCase().charCodeAt(0)] = handler_fn;
